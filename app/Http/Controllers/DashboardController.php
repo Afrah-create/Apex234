7646fb300db98134d265a10d31b7c84ca54b2d61
kamwadalex <kamwadalex47@gmail.com>
2025-06-17 17:56:14 +0300

    finishing admin dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $userCount = User::count(); // Example data
+       // $userCount = User::count(); // Example data
         // You can fetch more data here for the dashboard
 
         return view('dashboard', compact('userCount'));
